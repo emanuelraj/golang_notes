e6d56b95a553002d681568b946f99b16a25e177e
allen <allen@doodhwala.net>
2018-03-21 22:27:04 +0530

    Added code for Main file

---
+++ main.go
@@ -0,0 +1,14 @@
+// main.go
+
+package main
+
+func main() {
+	//a := App{}
+	// You need to set your Username and Password here
+	//a.Initialize("root", "root", "doodh_test")
+	//a.Initialize("dbuser", "!@#$%^doodhw", "doodhwala.net:3306", "DOODHWALA_BCK_FEB13")
+	//a.Initialize("dbuser", "!@#$%^doodhw", "doodhwala.net:3306","doodhwala_live_03_01_2018")
+	//username:password@protocol(address)/dbname?param=value
+
+	//a.Run(":8000")
+}
